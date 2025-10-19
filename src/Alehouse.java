c55052d4e6157b33cad58367565ba16f1725cdc6
alha2310 <alha2310@student.miun.se>
2025-10-19 23:20:51 +0200

    detta är fucking svåraste jävla hor skit bajset jag någonsitt stött på. Ska fucking döda en anka för hor skiten inte blir effektiv jävla hor fitt kuk

--- src/Alehouse.java
+++ src/Alehouse.java
@@ -1,78 +1,62 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.Comparator;
 
 public class Alehouse {
 
-    ArrayList<String> residents ;
+    ArrayList<int[]> residents ;
     int myInterval;
     int numberOfResidents;
+    int lastPosition = 0;
+    ArrayList<Integer> friendsMet = new ArrayList<>();
 
 
 
-    public Alehouse(ArrayList<String> list, int myInterval, int numberOfResidents) {
+    public Alehouse(ArrayList<int[]> list, int myInterval, int numberOfResidents) {
         this.residents = list;
         this.myInterval = myInterval;
         this.numberOfResidents = numberOfResidents;
+        sortedAfterStartTime();
         checkMaximumFriendsForInterval();
     }
 
-    private int getMaxEndTime() {
-        Integer[] endTimes = new Integer[numberOfResidents];
-        for (int i = 0; i < numberOfResidents; i++) {
-            int endTime = getResidentsTime(i)[1];
-            endTimes[i] = endTime;
-        }
-        Arrays.sort(endTimes, Collections.reverseOrder());
 
-        return endTimes[0];
-    }
-
-    private int[] getResidentsTime(int i) {
-        String resident = residents.get(i);
-
-        String[] splitResident = resident.split(" ");
-
-        int startTime = Integer.parseInt(splitResident[0]);
-        int endTime = Integer.parseInt(splitResident[1]);
-        return new int[]{startTime, endTime};
+    private void sortedAfterStartTime() {
+        residents.sort(Comparator.comparingInt(a -> a[0]));
     }
 
     private void checkMaximumFriendsForInterval() {
-        int[] startTimes = new int[numberOfResidents];
-        int[] endTimes = new int[numberOfResidents];
-        ArrayList<Integer> friendsMet = new ArrayList<>();
 
-        for (int i= 0; i < numberOfResidents; i++) {
-            startTimes[i] = getResidentsTime(i)[0];
-            endTimes[i] = getResidentsTime(i)[1];
-        }
-
-        for (int i = 0; i < numberOfResidents; i++) {
-            int myStartTime = startTimes[i];
-            int myEndTime = myStartTime + myInterval;
-            int numberOfFriendsMetAtThatTime = 0;
+        int myStartTime = residents.getFirst()[0] - myInterval;
+        int myEndTime = myStartTime + myInterval;
+        lastPosition = myStartTime;
 
 
-            while (myStartTime < endTimes[i]) {
-                for (int j = i + 1; j < numberOfResidents; j++) {
-                    if (myStartTime <= getResidentsTime(j)[0] && myEndTime >= getResidentsTime(j)[0]) {
-                        numberOfFriendsMetAtThatTime++;
-                    } else if (getResidentsTime(j)[1] >= myStartTime && getResidentsTime(j)[1] <= myEndTime) {
-                        numberOfFriendsMetAtThatTime++;
+        for (int i=0; i < numberOfResidents; i++) {
+            if (myStartTime < lastPosition) {
+                myStartTime = residents.get(i)[0] - myInterval;
+                continue;
+            }
+            while (myStartTime <= residents.get(i)[1] + myInterval) {
+                int friendsMetAtInterval = 0;
+
+                for (int j=i+1; j < numberOfResidents; j++) {
+                    if (myStartTime <= residents.get(j)[0] && myEndTime >= residents.get(j)[0] ||
+                            residents.get(j)[1] >= myStartTime && residents.get(j)[1] <= myEndTime ||
+                            residents.get(j)[0] <= myStartTime && residents.get(j)[1] >= myEndTime) {
+                        friendsMetAtInterval++;
+
                     }
-                    myStartTime++;
-                    myEndTime++;
                 }
+                lastPosition = myEndTime;
+                myStartTime++;
+                friendsMet.add(friendsMetAtInterval);
+
             }
 
-                friendsMet.add(numberOfFriendsMetAtThatTime);
         }
-
-        Collections.sort(friendsMet);
-        Collections.reverse(friendsMet);
-//        System.out.println(friendsMet);
+        friendsMet.sort(Comparator.reverseOrder());
         System.out.println(friendsMet.getFirst());
+
     }
 
 }
